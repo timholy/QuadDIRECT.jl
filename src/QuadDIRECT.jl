413194c99dc51b470f4026ce240d93a277710f25
Tim Holy <tim.holy@gmail.com>
2018-02-04 14:27:46 -0600

    Add some convenience methods for analyze

--- src/QuadDIRECT.jl
+++ src/QuadDIRECT.jl
@@ -11,4 +11,37 @@ include("types.jl")
 include("util.jl")
 include("algorithm.jl")
 
+# Convenience methods
+function analyze(f, x0::AbstractVector{<:Real}, lower::AbstractVector{<:Real}, upper::AbstractVector{<:Real}; kwargs...)
+    inds = linearindices(x0)
+    linearindices(lower) == linearindices(upper) == inds || error("lengths must match")
+    splits = similar(x0, Vector{Float64})
+    for i in inds
+        lo, hi, xi = lower[i], upper[i], x0[i]
+        lo = max(xi-1, lo)
+        hi = min(xi+1, hi)
+        if lo == xi || hi == xi
+            xi = (lo+hi)/2
+        end
+        @assert(lo < xi < hi)
+        @assert isfinite(lo) && isfinite(xi) && isfinite(hi)
+        splits[i] = [lo, xi, hi]
+    end
+    return analyze(f, splits, lower, upper; kwargs...)
+end
+
+function analyze(f, x0::AbstractVector{<:Real}; kwargs...)
+    return analyze(f, x0, fill(-Inf, length(x0)), fill(Inf, length(x0)); kwargs...)
+end
+
+function analyze(f, lower::AbstractVector{<:Real}, upper::AbstractVector{<:Real}; kwargs...)
+    x0 = (lower .+ upper)./2
+    for i in eachindex(x0)
+        if !isfinite(x0[i])
+            x0[i] = clamp(0, lower[i], upper[i])
+        end
+    end
+    return analyze(f, x0, lower, upper; kwargs...)
+end
+
 end # module
