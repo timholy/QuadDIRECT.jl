70b4ebeee0db7373c58321db8799b4e25c985abf
Tim Holy <tim.holy@gmail.com>
2018-02-13 13:39:10 -0600

    Tweaks for more informative feedback in gams problems

--- test/gams.jl
+++ test/gams.jl
@@ -79,11 +79,14 @@ excludes = ["nonconvex_smooth_gams/nnls.gms",
 
 mode = "eval"   # any choice other than "optimize" tests that the objective can be evaluated
 
+terminating = false
 for dir in gamsdirs
+    terminating && break
     subdir = joinpath(gamsdir, dir)
     cd(subdir) do
         files = readdir()
         @showprogress 1 "Working on $subdir" for file in files
+            endswith(file, ".gms") || (println("skipping ", file); continue)
             fullfile = joinpath(subdir, file)
             exclude = false
             for excl in excludes
@@ -94,11 +97,13 @@ for dir in gamsdirs
             end
             exclude && continue
             basename, ext = splitext(file)
+            lower, upper, x0 = read_problemdata(joinpath(gamsdir, "problemdata", basename)*".problem.data")
+            local x0g
             try
-                modex = parsegams(Module, file)
+                modex, axs = parsegams(Module, file)
+                x0g = axs[1]  # initialization provided by the file
                 mod = eval(modex)
-                f = getfield(mod, :f)
-                lower, upper, x0 = read_problemdata(joinpath(gamsdir, "problemdata", basename)*".problem.data")
+                f = getfield(mod, :objective)
                 # # FIXME: we need a better way of setting the splits
                 if mode == "optimize"
                     fwrap = QuadDIRECT.CountedFunction(f)
@@ -106,20 +111,31 @@ for dir in gamsdirs
                     successes[file] = (value(minimum(root)), fwrap.evals)
                 else
                     val = Base.invokelatest(f, x0)
+                    if !isfinite(val)
+                        # Also try any initialization present in the GAMS file
+                        val = Base.invokelatest(f, x0g)
+                        if isfinite(val)
+                            println(fullfile, ": finite result from file initialization but not problemdata")
+                        else
+                            println("\nnot finite on ", fullfile)
+                            println()
+                            push!(failures, fullfile)
+                        end
+                    end
                     if isfinite(val)
                         successes[fullfile] = val
-                    else
-                        println("not finite on ", fullfile)
-                        push!(failures, fullfile)
                     end
                 end
             catch ex
                 if ex isa InterruptException
+                    terminating = true
                     break
                 end
                 push!(failures, fullfile)
-                println("failed on ", fullfile)
+                println("\nfailed on ", fullfile)
+                println("problemdata axes ", Compat.axes(x0), ", and from the file it's ", Compat.axes(x0g))
                 showerror(STDOUT, ex)
+                println()
             end
         end
     end
